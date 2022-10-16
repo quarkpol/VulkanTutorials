5ea860cd19e810dc208411bbbcf0ea84a7d64667
Mariusz Rokicki <mrokicki@onet.pl>
2022-10-16 20:56:34 +0200

    Finished part 2 of the Vulkan tutorial. (https://www.youtube.com/watch\?v\=_riranMmtvI)

---
+++ VulkanGameEngine/compile.bat
@@ -0,0 +1,3 @@
+D:\VulkanSDK\1.3.224.1\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
+D:\VulkanSDK\1.3.224.1\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+pause
---
+++ VulkanGameEngine/lve_pipeline.cpp
@@ -0,0 +1,41 @@
+
+#include "lve_pipeline.hpp"
+
+// std
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+
+namespace lve {
+
+	std::vector<char> LvePipeline::readFile(const std::string& filepath) {
+		std::ifstream file(filepath, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file: " + filepath);
+
+		}
+
+		size_t fileSize = static_cast<size_t>(file.tellg());
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+		return buffer;
+	}
+
+	LvePipeline::LvePipeline(const std::string& vertFilepath, const std::string& fragFilepath) {
+		createGraphicsPipeline(vertFilepath, fragFilepath);
+	}
+
+	void LvePipeline::createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath) {
+
+		auto vertCode = readFile(vertFilepath);
+		auto fragCode = readFile(fragFilepath);
+
+		std::cout << "Vertex Shader Code Size: " << vertCode.size() << std::endl;
+		std::cout << "Fragment Shader Code Size: " << fragCode.size() << std::endl;
+	}
+}
